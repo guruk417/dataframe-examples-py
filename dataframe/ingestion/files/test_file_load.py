4847f42ac2811978e4de2ed243fe900140f3c588
Guru <guru417@gmail.com>
2021-08-14 18:06:28 -0500

    Initial commit

---
+++ dataframe/ingestion/files/test_file_load.py
@@ -0,0 +1,41 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, IntegerType, BooleanType,DoubleType
+import os.path
+import yaml
+
+if __name__ == '__main__':
+    spark = SparkSession\
+            .builder\
+            .appName('Test File Load')\
+            .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.4')\
+            .getOrCreate()
+
+    spark.sparkContext.setLogLevel('ERROR')
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    # Setup spark to use s3
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+
+    fin_schema = StructType()\
+                 .add('id',IntergerType(),True)\
+                 .add("has_debt", BooleanType(), True) \
+                 .add("has_financial_dependents", BooleanType(), True) \
+                 .add("has_student_loans", BooleanType(), True) \
+                 .add("income", DoubleType(), True)
+
+    fin_df = spark.read()\
+            .option("header", "false")\
+            .option("delimiter",',')\
+            .format("csv")
+            .load("s3a:\\" + app_conf["s3_conf"]["s3_bucket"])
+    spark.stop()
