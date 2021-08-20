366fdaaf394f1d533a01f7288b423e860e2fcadb
Guru <guru417@gmail.com>
2021-08-20 14:37:14 -0500

    Initial commit

--- dataframe/ingestion/files/test_sc_load.py
+++ dataframe/ingestion/files/test_sc_load.py
@@ -55,3 +55,23 @@ if __name__ == '__main__':
     #    .mode('overwrite').csv("s3a://" + app_conf['s3_conf']['s3_bucket']+ "/scop")
     # Stop Spark Session
     spark.stop()
+from pyspark.sql import SparkSession
+
+spark = SparkSession\
+    .builder\
+    .appName('Pyspark Withcolumn')\
+    .getOrCreate()
+
+data = [('James','','Smith','1991-04-01','M',3000),
+  ('Michael','Rose','','2000-05-19','M',4000),
+  ('Robert','','Williams','1978-09-05','M',4000),
+  ('Maria','Anne','Jones','1967-12-01','F',4000),
+  ('Jen','Mary','Brown','1980-02-17','F',-1)]
+
+columns = ["firstname","middlename","lastname","dob","gender","salary"]
+
+df = spark.createDataFrame(data,columns)
+
+# df.withColumn('salary', col('salary').cast('Integer'))
+
+df.withColumn("salary",col("salary").cast("Integer")).show()
