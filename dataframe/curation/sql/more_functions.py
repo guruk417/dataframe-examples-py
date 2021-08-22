6b8fb8571f3a5b79f2b2d5f821641f7622d6d287
Guru <guru417@gmail.com>
2021-08-21 22:16:10 -0500

    Initial commit

--- dataframe/curation/sql/more_functions.py
+++ dataframe/curation/sql/more_functions.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 
     people_df.show()
     people_df.createOrReplaceTempView("people")
-    spark.sql("select avg(weightInLbs) from people group by firstName").show()
+    spark.sql("select avg(first((weightInLbs)) from people group by firstName").show()
     '''
     people_df.groupBy("firstName").agg(first("weightInLbs")).show()
     people_df.groupBy(trim(lower(col('firstName')))).agg(first("weightInLbs")).show()
