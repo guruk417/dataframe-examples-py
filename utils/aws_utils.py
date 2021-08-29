78a2b9674b853810fff257faf3a09c1df0d4e7bd
Guru <guru417@gmail.com>
2021-08-28 20:38:41 -0500

    Initial commit

--- utils/aws_utils.py
+++ utils/aws_utils.py
@@ -13,3 +13,6 @@ def get_mysql_jdbc_url(mysql_config: dict):
     port = mysql_config["mysql_conf"]["port"]
     database = mysql_config["mysql_conf"]["database"]
     return "jdbc:mysql://{}:{}/{}?autoReconnect=true&useSSL=false".format(host, port, database)
+
+
+
