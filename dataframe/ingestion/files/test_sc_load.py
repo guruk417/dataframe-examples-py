14673f9cc3f53c382d337c60346a3ad07893fe3a
Guru <guru417@gmail.com>
2021-08-31 16:41:05 -0500

    Initial commit

--- dataframe/ingestion/files/test_sc_load.py
+++ dataframe/ingestion/files/test_sc_load.py
@@ -1,62 +1,43 @@
 from pyspark.sql import SparkSession
-import os.path
+import os
 import yaml
-from pyspark.sql.functions import col, when
-from pyspark.sql.types import StructType, IntegerType, StringType
+from pyspark.sql.functions import col, countDistinct
 
-if __name__ == '__main__':
-    # Start Spark Session
-    spark = SparkSession \
-        .builder \
-        .appName('test sc file load') \
-        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.4') \
-        .getOrCreate()
-    #        .master('local[*]') \
-    spark.sparkContext.setLogLevel('Error')
-    '''
-    # Define current path of application config path and secrets path
+if __name__ != "__main__":
+    # Create spark session
+    spark = SparkSession.builder.appName("Pyspark Example").getOrCreate()
+
+    # Access application yaml file for configuration and secrete file
+    spark.sparkContext.setLogLevel('ERROR')
+
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir + '/../../../' + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + '/../../../' + "secrets")
+    app_config_path = os.path.abspath(current_dir + "/../../../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../../../" + ".secrets")
 
-    # open and load application config and secret files
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
     secret = open(app_secrets_path)
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
 
-    # Set up to use S3
-    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
-    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
-    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
-
-    sc_schema = StructType() \
-        .add('SCA-POSTAL-ID', IntegerType(), True) \
-        .add('SCA-STATE-NAME', StringType(), True) \
-        .add('SCA-COUNTY-CODE', IntegerType(), True) \
-        .add('SCA-COUNTY-NAME', StringType(), True) \
-        .add('SCA-STATE-CODE', StringType(), True)
-
-    sc_read = spark \
+    sells_file = spark \
         .read \
-        .option('header', 'true') \
-        .csv("s3a://" + app_conf['s3_conf']['s3_bucket'] + "/SC_DB.csv")
-    #        .schema(sc_schema) \
-    sc_read.select(col('SCA-STATE-NAME').alias('State'), col('SCA-COUNTY-NAME').alias('County')) \
-        .where(col('SCA-STATE-NAME') == 'ILLINOIS').show()
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/sellers.csv")
+    prod_file = spark \
+        .read \
+        .option("header", "true") \
+        .option("interSchema", "true") \
+        .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/prod.csv")
+    sales_file = spark \
+        .read \
+        .option("header", "true") \
+        .option("interSchema", "true") \
+        .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/sales.csv")
 
-    # print('Num Of Partition:' + str(sc_read.rdd.getNumPartitions()))
-    # sc_read.groupBy('SCA-POSTAL-ID', 'SCA-COUNTY-NAME').count().show()
-    # Write dataframe into output file
-    # sc_read\
-    #    .write\
-    #    .partitionBy('SCA-POSTAL-ID')\
-    #    .option('header', 'true')\
-    #    .option('delimiter', '|')\
-    #    .mode('overwrite').csv("s3a://" + app_conf['s3_conf']['s3_bucket']+ "/scop")
-    # Stop Spark Session
-'''
+    print("Num Of orders {}".format(sales_file.count()))
+    print("Num of products {}").format(prod_file.count())
+    print("Num Of sells {}".format(sells_file.count()))
 
-
-
+    print("Num of Product Sold atleast {}".format(sales_file.agg(countDistinct(col("product_id")))))
     spark.stop()
