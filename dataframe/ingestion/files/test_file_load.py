0cd0f0ec4d86591a53667317342a65f26287d1f0
Guru <guru417@gmail.com>
2021-08-31 20:34:52 -0500

    Initial commit

--- dataframe/ingestion/files/test_file_load.py
+++ dataframe/ingestion/files/test_file_load.py
@@ -33,7 +33,24 @@ if __name__ == '__main__':
         .format("csv") \
         .load("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/sellers.csv")
 
+    prod_file = spark \
+        .read \
+        .option("header", "true") \
+        .option("interSchema", "true") \
+        .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/prod.csv")
+    sales_file = spark \
+        .read \
+        .option("header", "true") \
+        .option("interSchema", "true") \
+        .csv("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/sales.csv")
+
     print("Num Of sells {}".format(sells_file.count()))
+
+    print("Num Of orders {}".format(sales_file.count()))
+
+    print("Num of products {}").format(prod_file.count())
+
+
 '''
     fin_schema = StructType() \
         .add('id',  IntegerType(),  True) \
